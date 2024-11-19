9040a46672f1945168162a45c46c2f78044f0d30
Max Pilipovic <84630222+MaxPilipovic@users.noreply.github.com>
2024-11-18 19:06:13 -0800

    Added AlumLinx to middle of page to confirm tailwind css was working

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,8 +1,12 @@
+import '../index.css'; 
+
 function Home() {
     return (
-      <div>
-        <h1>Welcome to AlumLinx!</h1>
-        <p>Type shittt</p>
+      <div className="home-container">
+        <div className = "home-text">
+          <h1 className = "home-title">AlumLinx Coming Soon</h1>
+          <p>Type shittt</p>
+        </div>
       </div>
     );
   }
