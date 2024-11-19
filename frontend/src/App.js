b2b0d3d110210ed058ecb69e1fc8ab313e4d69e9
Max Pilipovic <84630222+MaxPilipovic@users.noreply.github.com>
2024-11-18 18:55:18 -0800

    Fixed comments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Landing from './pages/Landing'; // Import the Home component
-// Import other components as needed (e.g., Profile, Jobs)
+import Landing from './pages/Landing'; //Landing page
+//Import other profiles etc
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
-        {/* Add more routes for other pages like Profile, Jobs, etc */}
+        {/*Add routes here*/}
       </Routes>
     </Router>
   );
