1a7852363055fc629bcfbafc0155ca9e62fc68f0
Zarif Jabiullah Nafee <105160138+znafee@users.noreply.github.com>
2024-12-11 15:16:31 -0600

    Login
    Added some more stuff like login to the page, doesn't do anything so has no function, cleaned up the file a bit more.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-import Home from './pages/Landing';
+import {Home, Login} from './pages/Landing';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <div className="main-container">
+      <Home />
+      <Login />
+    </div>
   </React.StrictMode>
 );
 
--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,15 +1,51 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import '../index.css'; 
 
-function Home() {
+
+export function Home() {
     return (
       <div className="home-container">
         <div className = "home-text">
-          <h1 className = "home-title">AlumLinx Coming Soon</h1>
-          <p>Type shittt</p>
+          <h1 className = "home-title">AlumLinx</h1>
+          <p>Stay Linked with Greek Alumnis</p>
         </div>
       </div>
     );
   }
   
-  export default Home;
+export function Login() {
+  return (
+    <div className="login-container">
+      <div className="form-container">
+        <div className="input-wrapper">
+            <input
+              name="username"
+              id="username"
+              className="login-input"
+              placeholder=" "
+              type="text"
+              required
+            />
+         <label htmlFor="username" className="floating-label">
+              Username
+          </label>
+        </div>
+        <div className="input-wrapper">
+            <input
+              name="password"
+              id="password"
+              className="login-input"
+              placeholder=" "
+              type="password"
+              required
+            />
+            <label htmlFor="password" className="floating-label">
+              Password
+            </label>
+          </div>
+         {/* Login Button */}
+         <button className="login-button">Log in</button>
+      </div>
+    </div>
+  );
+}
